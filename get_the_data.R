226f3dd1b38f9b6f4911aa87c5a50a0896f381d5
DaveParr <8363743+DaveParr@users.noreply.github.com>
2023-03-21 12:28:09 +0000

    Use original get the data script from R For Datascience

---
+++ get_the_data.R
@@ -0,0 +1,19 @@
+# Originally from https://github.com/rfordatascience/tidytuesday/blob/master/data/2023/2023-01-31/readme.md#get-the-data-here
+# Get the Data
+
+# Read in with tidytuesdayR package
+# Install from CRAN via: install.packages("tidytuesdayR")
+# This loads the readme and all the datasets for the week of interest
+
+# Either ISO-8601 date or year/week works!
+
+tuesdata <- tidytuesdayR::tt_load("2023-01-31")
+tuesdata <- tidytuesdayR::tt_load(2023, week = 5)
+
+cats_uk <- tuesdata$cats_uk
+cats_uk_reference <- tuesdata$cats_uk_reference
+
+# Or read in the data manually
+
+cats_uk <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-31/cats_uk.csv")
+cats_uk_reference <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-31/cats_uk_reference.csv")
